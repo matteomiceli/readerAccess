6a7d81e3e5df3bb750ee0a789fb886d916247f1e
matteomiceli <omatteomiceli@gmail.com>
2023-01-09 01:47:32 -0800

    file device handler and demo file

--- src/services/deviceHandler.ts
+++ src/services/deviceHandler.ts
@@ -1,8 +1,27 @@
+import { device } from "./types.js";
+
 // file handler goes here
 export class DeviceHandler {
-  device = "fart";
+  device: device | null = null;
+  handle: null | FileSystemDirectoryHandle = null;
+
+  async init() {
+    this.handle = await this.pickDir();
+    this.handle.requestPermission({ mode: "readwrite" });
+    this.device = this.getDeviceType(this.handle.name);
+  }
 
   async pickDir() {
-    await window.showDirectoryPicker();
+    return window.showDirectoryPicker();
+  }
+
+  getDeviceType(name: string): device | null {
+    if (name.toLowerCase().includes("kobo")) {
+      return device.kobo;
+    }
+    if (name.toLowerCase().includes("kindle")) {
+      return device.kindle;
+    }
+    return device.unknown;
   }
 }
