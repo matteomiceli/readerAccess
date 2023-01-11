dfc7edda4580435b721539695fd69b17c96a8a3a
matteomiceli <omatteomiceli@gmail.com>
2023-01-10 16:41:13 -0800

    files list

--- src/services/deviceHandler.ts
+++ src/services/deviceHandler.ts
@@ -3,12 +3,17 @@ import { device } from "./types.js";
 // file handler goes here
 export class DeviceHandler {
   device: device | null = null;
+  name: string = "";
   handle: null | FileSystemDirectoryHandle = null;
+  files: { [key: string]: FileSystemDirectoryHandle | FileSystemFileHandle } =
+    {};
 
   async access() {
     this.handle = await this.pickDir();
     this.handle.requestPermission({ mode: "readwrite" });
+    this.name = this.handle.name;
     this.device = this.getDeviceType(this.handle.name);
+    await this.getFiles();
   }
 
   async pickDir() {
@@ -23,5 +28,15 @@ export class DeviceHandler {
       return device.kindle;
     }
     return device.unknown;
+  }
+
+  async getFiles() {
+    if (!this.handle?.values()) {
+      return;
+    }
+
+    for await (const file of this.handle.values()) {
+      this.files[file.name] = file;
+    }
   }
 }
