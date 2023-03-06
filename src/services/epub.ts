55302417f93784c486c62db6cb6fb6704875aad5
matteomiceli <omatteomiceli@gmail.com>
2023-03-06 00:24:17 -0800

    retrieving basic epub metadata from opf

--- src/services/epub.ts
+++ src/services/epub.ts
@@ -9,13 +9,18 @@ export async function parseEpub(file: File) {
   const unzipped = await new zip.ZipReader(zipReader).getEntries();
   const writer = new zip.TextWriter();
 
-  const parsed: EpubMetaData = {};
-
   const opf = await unzipped
     .filter((file) => {
       return file.filename.includes("opf");
     })[0]
     .getData(writer);
 
-  return opf;
+  const parser = new DOMParser();
+  const xmlMeta = parser
+    .parseFromString(opf, "text/xml")
+    .getElementsByTagName("metadata")[0];
+
+  return {
+    author: xmlMeta.getElementsByTagName("dc:creator")?.[0].innerHTML,
+  };
 }
