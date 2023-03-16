85e67abaa8f27dde4e7d3eec8f6c33559b005873
matteomiceli <omatteomiceli@gmail.com>
2023-03-15 23:34:24 -0700

    refactor

--- src/services/epub.ts
+++ src/services/epub.ts
@@ -1,4 +1,5 @@
 import * as zip from "@zip.js/zip.js";
+import * as xml from "../utils/parseXML";
 
 interface EpubMetaData {
   title?: string;
@@ -11,13 +12,7 @@ interface EpubMetaData {
 
 /** Pulls metadata out of an epub's *.opf file */
 export async function getEpubMetaData(file: File): Promise<EpubMetaData> {
-  if (file.type !== "application/epub+zip") {
-    throw new Error("File is not a valid epub");
-  }
-
-  const zipReader = new zip.ZipReader(new zip.BlobReader(file));
-  const unzipped = await zipReader.getEntries();
-  await zipReader.close();
+  const unzipped = await unpackEpub(file);
   const writer = new zip.TextWriter();
 
   const opf = await unzipped
@@ -25,6 +20,7 @@ export async function getEpubMetaData(file: File): Promise<EpubMetaData> {
       return file.filename.includes("opf");
     })
     ?.getData(writer);
+
   if (!opf) {
     throw new Error("Epub does not contain opf metadata");
   }
@@ -33,50 +29,28 @@ export async function getEpubMetaData(file: File): Promise<EpubMetaData> {
   const xmlData = parser.parseFromString(opf, "text/xml");
 
   return {
-    title: getContentByTag(xmlData, "dc:title"),
-    author: getContentByTag(xmlData, "dc:creator"),
-    authorFileAs: getAttributeValueByName(xmlData, "dc:creator", "opf:file-as"),
-    description: getContentByTag(xmlData, "dc:description"),
-    isbn: getElementByAttributeValue(xmlData, "dc:identifier", "ISBN")
+    title: xml.getContentByTag(xmlData, "dc:title"),
+    author: xml.getContentByTag(xmlData, "dc:creator"),
+    authorFileAs: xml.getAttributeValueByName(
+      xmlData,
+      "dc:creator",
+      "opf:file-as"
+    ),
+    description: xml.getContentByTag(xmlData, "dc:description"),
+    isbn: xml.getElementByAttributeValue(xmlData, "dc:identifier", "ISBN")
       ?.innerHTML,
-    cover: getEpubCoverPath(xmlData),
+    cover: xml.getEpubCoverPath(xmlData),
   };
 }
 
-function getContentByTag(xml: Document, tagName: string) {
-  return xml.getElementsByTagName(tagName)?.[0].innerHTML;
-}
-
-function getAttributeValueByName(
-  xml: Document,
-  tagName: string,
-  attributeName: string
-) {
-  return Object.values(xml.getElementsByTagName(tagName)?.[0].attributes).find(
-    (attr) => attr.name === attributeName
-  )?.value;
-}
-
-function getEpubCoverPath(xml: Document) {
-  const coverTag = xml.getElementsByName("cover")?.[0];
-
-  if (!coverTag) {
-    return;
+async function unpackEpub(file: File) {
+  if (file.type !== "application/epub+zip") {
+    throw new Error("File is not a valid epub");
   }
 
-  // Manifest ID
-  const coverId = Object.values(coverTag.attributes).find(
-    (attr) => attr.name === "content"
-  )?.value;
+  const zipReader = new zip.ZipReader(new zip.BlobReader(file));
+  const unzipped = await zipReader.getEntries();
+  await zipReader.close();
 
-  // Get cover path from manifest
-  return Object.values(
-    xml.getElementById(coverId || "")?.attributes || []
-  ).find((attr) => attr.name === "href")?.value;
-}
-
-function getElementByAttributeValue(xml: Document, tag: string, val: string) {
-  return Object.values(xml.getElementsByTagName(tag)).find((t) =>
-    Object.values(t.attributes).find((a) => a.value === val)
-  );
+  return unzipped;
 }
---
+++ src/utils/parseXML.ts
@@ -0,0 +1,41 @@
+export function getContentByTag(xml: Document, tagName: string) {
+  return xml.getElementsByTagName(tagName)?.[0].innerHTML;
+}
+
+export function getAttributeValueByName(
+  xml: Document,
+  tagName: string,
+  attributeName: string
+) {
+  return Object.values(xml.getElementsByTagName(tagName)?.[0].attributes).find(
+    (attr) => attr.name === attributeName
+  )?.value;
+}
+
+export function getEpubCoverPath(xml: Document) {
+  const coverTag = xml.getElementsByName("cover")?.[0];
+
+  if (!coverTag) {
+    return;
+  }
+
+  // Manifest ID
+  const coverId = Object.values(coverTag.attributes).find(
+    (attr) => attr.name === "content"
+  )?.value;
+
+  // Get cover path from manifest
+  return Object.values(
+    xml.getElementById(coverId || "")?.attributes || []
+  ).find((attr) => attr.name === "href")?.value;
+}
+
+export function getElementByAttributeValue(
+  xml: Document,
+  tag: string,
+  val: string
+) {
+  return Object.values(xml.getElementsByTagName(tag)).find((t) =>
+    Object.values(t.attributes).find((a) => a.value === val)
+  );
+}
